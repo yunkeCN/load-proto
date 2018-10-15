727606671be92e7d002daeebc6f7eab7a9fe1e9d
蒋津 <jiangj01@mysoft.com.cn>
2018-10-15 14:12:27 +0800

    split load git

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,15 @@
-import * as fs from 'fs';
-import * as glob from 'glob';
-import * as mkdirp from 'mkdirp';
 import * as path from 'path';
+import * as glob from 'glob';
 import * as protobufjs from 'protobufjs';
 import { Root } from 'protobufjs';
-import * as request from 'request';
-import * as unzip from 'unzip';
-import * as url from 'url';
-import { getArchiveUrls, getBranchLastCommitId } from "./git";
-import { loadFromJson } from "./loader";
+import { load } from '@yunke/load-git';
+import * as fsExtra from 'fs-extra';
 
 export { loadFromJson, createPackageDefinition } from './loader';
 
 const rmrf = require('rmrf');
 
-const CACHE_DIR = `${process.cwd()}/load-proto-cache`;
-
-interface IDownloadRes {
-  path: string;
-  dir: string;
-  url: string;
-}
-
-async function downloadItem(
-    urlStr: string,
-    dir: string = `${CACHE_DIR}/${global.Date.now()}_${Math.random()}`,
-    accessToken: string,
-): Promise<IDownloadRes> {
-  const urlObj = url.parse(urlStr, true);
-  const zipName = urlStr
-      .replace(/^https?:\/\//, '')
-      .replace(/\/repository\/.+$/, '')
-      .replace(/\//g, '_');
-  const filePath = `${dir}/${zipName}.zip`;
-
-  await new Promise((resolve, reject) => {
-    mkdirp(dir, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-
-  return new Promise<any>((resolve, reject) => {
-    request({
-      url: urlStr + (!!urlObj.query.private_token ? '' : `?private_token=${accessToken}`),
-      timeout: 30000,
-    })
-        .on('error', (err) => {
-          rmrf(dir);
-          reject(err);
-        })
-        .on('response', (res) => {
-          if (res.statusCode === 404) {
-            reject(new Error('系统错误，请联系管理员'));
-          } else if (res.statusCode !== 200) {
-            reject(new Error('服务器配置错误'));
-          }
-        })
-        .pipe(fs.createWriteStream(filePath))
-        .on('close', () => {
-          resolve({ path: filePath, dir, url: urlStr });
-        });
-  });
-}
-
-async function download(
-    urls: string[],
-    accessToken: string,
-    dir: string = `${CACHE_DIR}/${global.Date.now()}_${Math.random()}`,
-): Promise<IDownloadRes[]> {
-  return Promise.all(urls.map((item) => downloadItem(item, dir, accessToken)));
-}
-
-function unzipProcess(downloadResArr: IDownloadRes[]): Promise<string[]> {
-  return Promise.all(downloadResArr.map((downloadRes) => {
-    return new Promise<string>((resolve, reject) => {
-      const targetDir = path.dirname(downloadRes.path) + '/' + downloadRes.url
-          .replace(/^https?:\/\//, '')
-          .replace(/\/repository\/.+$/, '');
-      fs.createReadStream(downloadRes.path)
-          .pipe(unzip.Parse())
-          .on('entry', (entry) => {
-            if (entry.type === 'File') {
-              const filepath = `${targetDir}/${entry.path.split('/').slice(1).join('/')}`;
-              const dir = path.dirname(filepath);
-              mkdirp(dir, (err) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  entry.pipe(fs.createWriteStream(filepath));
-                }
-              });
-            }
-          })
-          .on('close', () => {
-            resolve(targetDir);
-          });
-    });
-  }));
-}
+const CACHE_DIR = `${process.cwd()}/.load-proto-cache`;
 
 async function pbjs(protoDirs: string[], includeDir: string): Promise<Root> {
   const protoFiles: string[] = [];
@@ -151,76 +59,46 @@ async function pbjs(protoDirs: string[], includeDir: string): Promise<Root> {
   });
 }
 
-const gitHttpReg = /https?:\/\/([^/]+)\/(.+)\.git/;
-const gitSshReg = /git@([^:]+):(.+)\.git/;
-
-function getUrlConfig(gitUrl: string): { host: string, repId: string } | null {
-  let host = '';
-  let repId = '';
-
-  let exec = gitHttpReg.exec(gitUrl);
-  if (exec) {
-    host = exec[1];
-    repId = exec[2];
-  }
-
-  if (!exec) {
-    exec = gitSshReg.exec(gitUrl);
-
-    if (exec) {
-      host = exec[1];
-      repId = exec[2];
-    }
-  }
-
-  if (exec) {
-    return { host, repId };
-  }
-  return null;
-}
-
 export async function loadProto(gitUrls: string[], branch: string, accessToken: string): Promise<Root> {
-  const archiveUrls: string[] = gitUrls
-      .map<string>((gitUrl) => {
-        const archiveUrls = getArchiveUrls(gitUrl, [branch]);
-        if (archiveUrls) {
-          return archiveUrls[0];
-        }
-        return '';
+  const loadRes = await Promise.all([
+    load({
+      url: 'https://git.myscrm.cn/golang/common.git',
+      branch: 'master',
+      accessToken,
+    })
+  ].concat(
+      gitUrls.map((gitUrl) => {
+        return load({ url: gitUrl, accessToken, branch });
       })
-      .filter((item) => !!item);
-  let tempDir;
-  let root: Root;
-  try {
-    const branchLastCommitId = await getBranchLastCommitId(gitUrls[0], branch, accessToken);
+  ));
+  const tempDir = `${CACHE_DIR}/${Math.random()}-${Date.now()}`;
 
-    const filePath = (branchLastCommitId ? `${CACHE_DIR}/${branchLastCommitId}` : undefined);
-    const jsonFilePath = (branchLastCommitId ? `${filePath}/json.json` : undefined);
-    if (!jsonFilePath || !fs.existsSync(jsonFilePath)) {
-      const downloadResArr = await download(
-          [
-            // 公共依赖仓库
-            'https://git.myscrm.cn/golang/common/repository/master/archive.zip',
-            ...archiveUrls,
-          ],
-          accessToken,
-          filePath,
-      );
-      tempDir = downloadResArr[0].dir;
-      let protoDirs = await unzipProcess(downloadResArr);
-      root = await pbjs(protoDirs.slice(1), tempDir);
-      if (jsonFilePath) {
-        fs.writeFileSync(jsonFilePath, JSON.stringify(root.toJSON({ keepComments: true })));
-      }
-    } else {
-      root = Root.fromJSON(require(jsonFilePath), new Root());
-    }
+  try {
+    await fsExtra.mkdirp(tempDir);
+
+    const copyDirs = await Promise.all(loadRes.map(async (res) => {
+      const dest = `${tempDir}/${path.relative(res.parentDir, res.path)}`;
+
+      await fsExtra.mkdirp(dest);
+
+      await new Promise((resolve, reject) => {
+        fsExtra.copy(res.path, dest, (err) => {
+          if (err) {
+            reject(err);
+          }
+          resolve();
+        });
+      });
+      return dest;
+    }));
+
+    const root = await pbjs(copyDirs.slice(1), tempDir);
+
+    await fsExtra.remove(tempDir);
+
+    return root;
   } catch (e) {
-    if (tempDir) {
-      rmrf(tempDir);
-    }
+    rmrf(tempDir);
     throw e;
   }
-
-  return root;
 }
